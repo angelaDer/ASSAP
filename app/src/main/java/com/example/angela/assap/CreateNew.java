965c40fe3ddd834cf0a695f9e3fd79c7a9aa9e92
angelaDer <35682671+angelaDer@users.noreply.github.com>
2019-08-17 17:27:56 +0200

    Create CreateNew.java
    CreateNew.java has been created

---
+++ app/src/main/java/com/example/angela/assap/CreateNew.java
@@ -0,0 +1,15 @@
+package com.example.angela.assap;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+
+public class CreateNew extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.createnew);
+
+    }
+}
