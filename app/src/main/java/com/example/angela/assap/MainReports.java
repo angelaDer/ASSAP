ccf5000e6d082074187aaccb58d2c3fc283e3f0d
angelaDer <35682671+angelaDer@users.noreply.github.com>
2019-08-17 17:28:34 +0200

    Update MainReports.java
    CreateNew.java has been created

--- app/src/main/java/com/example/angela/assap/MainReports.java
+++ app/src/main/java/com/example/angela/assap/MainReports.java
@@ -1,13 +1,28 @@
 package com.example.angela.assap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainReports extends AppCompatActivity {
+
+    ImageButton createNewButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.allreports);
+
+        createNewButton = (ImageButton) findViewById(R.id.createnew);
+        createNewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent createNewActivity = new Intent(MainReports.this, CreateNew.class);
+                startActivity(createNewActivity);
+            }
+        });
     }
 }
