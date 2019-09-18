d557ee09a94362c0aa97d0585fd75cd3e4c65477
angelaDer <35682671+angelaDer@users.noreply.github.com>
2019-09-18 21:07:08 +0200

    Działające zdjęcia

--- app/src/main/java/com/example/angela/assap/CreateNew.java
+++ app/src/main/java/com/example/angela/assap/CreateNew.java
@@ -1,28 +1,24 @@
 package com.example.angela.assap;
 
 import android.Manifest;
-import android.content.ContentValues;
 import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.support.v4.app.ActivityCompat;
 
 public class CreateNew extends AppCompatActivity {
 
-    private static final int PERMISSION_CODE = 1000;
-    private  static final int IMAGE_CAPTURE_CODE = 1001;
-
     ImageButton cameraButton;
     ImageView photo1;
-    Uri image_uri;
+    Intent intent;
+    public static final int RequestPermissionCode = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,54 +28,63 @@ public class CreateNew extends AppCompatActivity {
         photo1 = findViewById(R.id.photo1);
         cameraButton = findViewById(R.id.cameraButton);
 
+        EnableRuntimePermission();
+
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                    if (checkSelfPermission(Manifest.permission.CAMERA) ==
-                            PackageManager.PERMISSION_DENIED ||
-                            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
-                            PackageManager.PERMISSION_DENIED){
-                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-                        requestPermissions(permission, PERMISSION_CODE);
-                    }
-                    else{
-                        openCamera();
-                    }
-                }
-                else{
 
-                }
+                intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 7);
+
             }
-
-            private void openCamera() {
-                ContentValues values = new ContentValues();
-                values.put(MediaStore.Images.Media.TITLE, "New Picture");
-                values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
-                image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
-                startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
-            }
-
-            public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
-                switch (requestCode){
-                    case PERMISSION_CODE:{
-                        if (grantResults.length > 0 && grantResults[0] ==
-                                PackageManager.PERMISSION_GRANTED){
-                            openCamera();
-                        }
-                    }
-                }
-            }
-
-            protected  void onActivityResult(int requestCode, int resultCode, Intent data){
-                if (resultCode == RESULT_OK){
-                    photo1.setImageURI(image_uri);
-                }
-            }
-
         });
-
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 7 && resultCode == RESULT_OK) {
+
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+
+            photo1.setImageBitmap(bitmap);
+        }
+    }
+
+    public void EnableRuntimePermission(){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(CreateNew.this,
+                Manifest.permission.CAMERA))
+        {
+
+            Toast.makeText(CreateNew.this,"CAMERA permission allows us to Access CAMERA app", Toast.LENGTH_LONG).show();
+
+        } else {
+
+            ActivityCompat.requestPermissions(CreateNew.this,new String[]{
+                    Manifest.permission.CAMERA}, RequestPermissionCode);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int RC, String per[], int[] PResult) {
+
+        switch (RC) {
+
+            case RequestPermissionCode:
+
+                if (PResult.length > 0 && PResult[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(CreateNew.this,"Permission Granted, Now your application can access CAMERA.", Toast.LENGTH_LONG).show();
+
+                } else {
+
+                    Toast.makeText(CreateNew.this,"Permission Canceled, Now your application cannot access CAMERA.", Toast.LENGTH_LONG).show();
+
+                }
+                break;
+        }
+    }
+
 }
